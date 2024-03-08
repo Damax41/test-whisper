35e222d40682ca86addb7d244f64e1f599a6ac1d
Damax41 <david.dias40@hotmail.fr>
2024-03-08 11:14:36 +0100

    commit

--- whisper-test.py
+++ whisper-test.py
@@ -1,21 +1,41 @@
+import os
 import gc
+import torch
 import whisper
 
 def retranscript(audio_path, model_name="base"):
+    """
+    Retranscribes the given audio file using the specified model.
+
+    Args:
+        audio_path (str): The path to the audio file.
+        model_name (str, optional): The name of the model to use for transcription. Defaults to "base".
+
+    Returns:
+        tuple: A tuple containing the transcribed text and a boolean indicating the success of the transcription.
+    """
     global success
     success = False
 
+    # With the Whisper library
     try:
-        modelLoaded = whisper.load_model(name=model_name)
-        result = modelLoaded.transcribe(audio_path)
+        device = "cuda" if torch.cuda.is_available() else "cpu"
+        model = whisper.load_model(model_name).to(device)
+        options = whisper.DecodingOptions(fp16=False)
+        result = model.transcribe(
+            audio_path,
+            temperature=0.0,
+            word_timestamps=False,
+            decode_options=options
+        )
 
-        del modelLoaded
+        del model
         gc.collect()
 
         result = result["text"]
 
         success = True
-    
+
     except Exception as e:
         result = str(e)
 
